3c4f9945fe36804ea5f16723dbb67ce1952b5402
Eduardo Ribeiro <dudub9go@hotmail.com>
2024-04-27 20:05:10 -0300

    Adicionar tema global

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import GlobalStyles from '../src/app/styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from '../src/app/styles/theme'
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
