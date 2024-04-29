12aa0e76770b276323adf48bef5286878e820b00
Eduardo Ribeiro <dudub9go@hotmail.com>
2024-04-29 11:22:32 -0300

    Adicionar componente de logo com testes

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,23 @@
+import { Meta } from '@storybook/react'
+import Logo, { LogoProps } from '.'
+
+export default {
+  title: 'Logo',
+  component: Logo,
+  argTypes: {
+    color: {
+      description: 'Colors of the logo',
+      control: { type: 'select' },
+      options: ['black', 'white']
+    }
+  }
+} as Meta
+
+const LogoTemplate = (args: LogoProps) => <Logo {...args} />
+
+// Usado any apenas para tirar o erro de tipagem do args
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const Default = LogoTemplate.bind({}) as any
+Default.args = {
+  color: 'white'
+}
