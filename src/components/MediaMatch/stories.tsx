e110e25f55c56deff965c8ae422bd58144eb3ffc
Eduardo Ribeiro <dudub9go@hotmail.com>
2024-07-27 11:31:21 -0300

    Add MediaMatch component for styled in desktop and mobile

---
+++ src/components/MediaMatch/stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react'
+import MediaMatch from '.'
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch
+} as Meta
+
+export const Desktop: StoryObj<typeof MediaMatch> = {
+  render: () => <MediaMatch $greaterThan="medium">Only on Desktop</MediaMatch>
+}
+
+export const Mobile: StoryObj<typeof MediaMatch> = {
+  render: () => <MediaMatch $lessThan="medium">Only on Mobile</MediaMatch>
+}
+
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1'
+  }
+}
+
+Desktop.parameters = {
+  viewport: {
+    defaultViewport: 'desktop'
+  }
+}
