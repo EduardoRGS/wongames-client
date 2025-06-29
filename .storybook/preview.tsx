e30a26d20d8177a27985885d726a9ee797c9854f
Eduardo Ribeiro <dudub9go@hotmail.com>
2025-06-28 23:07:47 -0300

    ajuste de backgorund color do storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,16 @@ import GlobalStyles from '../src/app/styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from '../src/app/styles/theme'
 
+export const parameters = {
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      { name: 'won-light', value: theme.colors.white },
+      { name: 'won-dark', value: theme.colors.black }
+    ]
+  }
+}
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
