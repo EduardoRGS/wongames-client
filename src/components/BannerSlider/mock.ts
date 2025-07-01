fc0f2cca3f9e109d4169a0308ab3b2ec77b4a5a6
Eduardo Ribeiro <dudub9go@hotmail.com>
2025-06-30 22:56:41 -0300

    Mock de dados do banner slider

---
+++ src/components/BannerSlider/mock.ts
@@ -0,0 +1,26 @@
+const banners = [
+  {
+    img: 'img/banner-won.png',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'img/rd2-background.png',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  {
+    img: 'img/rd2-img.png',
+    title: 'Defy death 3',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+export default banners
