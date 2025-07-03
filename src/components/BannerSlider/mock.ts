089af0d774896d3466b4a724ea59c9e1c73159c3
Eduardo Ribeiro <dudub9go@hotmail.com>
2025-07-02 22:31:23 -0300

    Ajustar mocks

--- src/components/BannerSlider/mock.ts
+++ src/components/BannerSlider/mock.ts
@@ -1,6 +1,6 @@
 const banners = [
   {
-    img: 'img/banner-won.png',
+    img: '/img/banner-won.png',
     title: 'Defy death 1',
     subtitle: '<p>Play the new <strong>CrashLands</strong> season',
     buttonLabel: 'Buy now',
@@ -8,14 +8,14 @@ const banners = [
     ribbon: 'Bestselling'
   },
   {
-    img: 'img/rd2-background.png',
+    img: '/img/rd2-background.png',
     title: 'Defy death 2',
     subtitle: '<p>Play the new <strong>CrashLands</strong> season',
     buttonLabel: 'Buy now',
     buttonLink: '/games/defy-death'
   },
   {
-    img: 'img/rd2-img.png',
+    img: '/img/rd2-img.png',
     title: 'Defy death 3',
     subtitle: '<p>Play the new <strong>CrashLands</strong> season',
     buttonLabel: 'Buy now',
--- src/components/GameCardSlider/mock.ts
+++ src/components/GameCardSlider/mock.ts
@@ -3,7 +3,7 @@ const gameCardSliderMock = [
     title: 'Population Zero',
     slug: 'population-zero',
     developer: 'Rockstar Games',
-    image: 'img/rd2-img.png',
+    image: '/img/rd2-img.png',
     price: 'R$ 235,00',
     promotionalPrice: 'R$ 195,00'
   },
@@ -11,7 +11,7 @@ const gameCardSliderMock = [
     title: 'Population Zero',
     slug: 'population-zero',
     developer: 'Rockstar Games',
-    image: 'img/rd2-background.png',
+    image: '/img/rd2-background.png',
     price: 'R$ 230,00',
     promotionalPrice: 'R$ 190,00'
   },
@@ -19,7 +19,7 @@ const gameCardSliderMock = [
     title: 'Population Zero',
     slug: 'population-zero',
     developer: 'Rockstar Games',
-    image: 'img/rd2-img.png',
+    image: '/img/rd2-img.png',
     price: 'R$ 225,00',
     promotionalPrice: 'R$ 180,00'
   },
@@ -27,7 +27,7 @@ const gameCardSliderMock = [
     title: 'Population Zero',
     slug: 'population-zero',
     developer: 'Rockstar Games',
-    image: 'img/rd2-background.png',
+    image: '/img/rd2-background.png',
     price: 'R$ 220,00',
     promotionalPrice: 'R$ 170,00'
   },
@@ -35,7 +35,7 @@ const gameCardSliderMock = [
     title: 'Population Zero',
     slug: 'population-zero',
     developer: 'Rockstar Games',
-    image: 'img/rd2-background.png',
+    image: '/img/rd2-background.png',
     price: 'R$ 220,00',
     promotionalPrice: 'R$ 170,00'
   },
@@ -43,7 +43,7 @@ const gameCardSliderMock = [
     title: 'Population Zero',
     slug: 'population-zero',
     developer: 'Rockstar Games',
-    image: 'img/rd2-background.png',
+    image: '/img/rd2-background.png',
     price: 'R$ 220,00',
     promotionalPrice: 'R$ 170,00'
   }
