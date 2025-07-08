e836aa479f68d6046bf6ca02a91512ae0049a0b7
Eduardo Ribeiro <dudub9go@hotmail.com>
2025-07-08 15:31:51 -0300

    Adicionar componentes na home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-'use client'
-
 import HomePage from './templates/Home'
+import gameCardSliderMock from '@/components/GameCardSlider/mock'
+import highlightMock from '@/components/Highlight/mock'
+import bannerMock from '@/components/BannerSlider/mock'
+
+const homeMoke = {
+  banners: bannerMock,
+  newGames: gameCardSliderMock,
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: gameCardSliderMock,
+  upcomingGames: gameCardSliderMock,
+  upcomingHighlight: highlightMock,
+  upcomingMoreGames: gameCardSliderMock,
+  freeGamesHighlight: highlightMock,
+  freeGames: gameCardSliderMock
+}
 
 export default function Home() {
-  return <HomePage />
+  return <HomePage {...homeMoke} />
 }
--- src/app/templates/Home/index.tsx
+++ src/app/templates/Home/index.tsx
@@ -1,38 +1,74 @@
+'use client'
+
 import Menu from '@/components/Menu'
 import { Container } from '@/components/Container'
 import Footer from '@/components/Footer'
 import Heading from '@/components/Heading'
+import { BannerProps } from '@/components/Banner'
+import GameCardSlider from '@/components/GameCardSlider'
+import Highlight, { HighlightProps } from '@/components/Highlight'
+import BannerSlider from '@/components/BannerSlider'
+import { GameCardProps } from '@/components/GameCard'
 
-import * as S from './styles'
+export type HomeTemplateProps = {
+  banners: BannerProps[]
+  newGames: GameCardProps[]
+  mostPopularHighlight: HighlightProps
+  mostPopularGames: GameCardProps[]
+  upcomingGames: GameCardProps[]
+  upcomingHighlight: HighlightProps
+  upcomingMoreGames: GameCardProps[]
+  freeGamesHighlight: HighlightProps
+  freeGames: GameCardProps[]
+}
 
-const Home = () => (
+const Home = ({
+  banners,
+  newGames,
+  mostPopularHighlight,
+  mostPopularGames,
+  upcomingGames,
+  upcomingHighlight,
+  upcomingMoreGames,
+  freeGamesHighlight,
+  freeGames
+}: HomeTemplateProps) => (
   <section>
     <Container>
       <Menu />
+      <BannerSlider items={banners} />
     </Container>
 
     <Container>
       <Heading $lineLeft lineColor="secondary" color="black">
         News
       </Heading>
+      <GameCardSlider items={newGames} />
     </Container>
 
     <Container>
       <Heading $lineLeft lineColor="secondary">
         Most Popular
       </Heading>
+      <Highlight {...mostPopularHighlight} />
+      <GameCardSlider items={mostPopularGames} />
     </Container>
 
     <Container>
       <Heading $lineLeft lineColor="secondary">
         Upcoming
       </Heading>
+      <GameCardSlider items={upcomingGames} />
+      <Highlight {...upcomingHighlight} />
+      <GameCardSlider items={upcomingMoreGames} />
     </Container>
 
     <Container>
       <Heading $lineLeft lineColor="secondary">
         Free Games
       </Heading>
+      <Highlight {...freeGamesHighlight} />
+      <GameCardSlider items={freeGames} />
     </Container>
 
     <Container>
